3d71036ed4c1f47bb9b1a8de21d3e72c403ddfc1
Sebastian Villa Lopez <85509028+SebasVillaLo@users.noreply.github.com>
2022-08-03 12:18:23 -0500

    Delete 0x0E-ES6_basic directory

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++
@@ -1,6 +0,0 @@
-export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees);
-  const employeesList = [];
-  for (const employee of employees) employeesList.push(...employee);
-  return employeesList;
-}
--- 0x0E-ES6_basic/2-arrow.js
+++
@@ -1,8 +0,0 @@
-export default function getNeighborhoodsList() {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  this.addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
-  };
-}
